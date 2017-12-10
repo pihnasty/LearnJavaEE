8afd70fdc41c12d8a787117ef47aa719f0f18482
POM <pom7@bk.ru>
2017-12-10 12:17:47 +0200

    26 - Java EE. Дизaйн глaвнoй cтpaницы. Разбор д/з

---
+++ src/main/java/beans/Genre.java
@@ -0,0 +1,24 @@
+package beans;
+
+public class Genre {
+    
+    private String name;
+
+    public Genre() {
+    }
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+
+}
